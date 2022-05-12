2755add7527abb83d383c33af2c907067730ceea
Mofles11 <luisangelramosmejia11@gmail.com>
2022-05-11 21:31:27 -0500

    agregar docker y variables de entorno

---
+++ src/components/NavBar.js
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom';
+
+export default function NavBar () {
+    return(
+        <nav>
+            <a to='/'>f</a>
+            <a to='/'>g</a>
+            <a to='/'>g</a>
+            <a to='/'>g</a>
+        </nav>
+    );
+}
